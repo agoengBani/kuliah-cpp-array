a03b9f183e845c3b310d36a7ddb3cf2cbcad0ac9
Muhammad Agung Sya'bani <119343277+agoengBani@users.noreply.github.com>
2023-05-20 11:46:29 +0700

    Update pertemuan3.cpp

--- pertemuan3.cpp
+++ pertemuan3.cpp
@@ -2,26 +2,70 @@
 using namespace std;
 
 int main() {
+	// CRUD
 	
-	int length;
+	// Create
+	int length, i;
 	
 	cout << "Masukkan input: ";
 	cin >> length;
 	
 	string arr[length];
 	
- 	for (int i = 0; i < length; i++) {
+ 	for (i = 0; i < length; i++) {
  		cout << "Input data ke " << i+1 << ": ";
 		cin >> arr[i];
 	}
 	
+	
+	// Read
 	cout << "Output: ";
-	for (int i = 0; i < length; i++) {
+	for (i = 0; i < length; i++) {
 		if (i > 0) {
 			cout << ", ";
 		}
 		cout << arr[i] << "";
 	}
+	cout << endl << endl;
+	
+	
+	// Update
+	cout << "Pilih indeks data yang akan diubah = ";
+	cin >> i;
+	
+	i = i-1;
+	
+	cout << "Ubah data: ";
+	cin >> arr[i];
+	
+	cout << "Output: ";
+	for (i = 0; i < length; i++) {
+		if (i > 0) {
+			cout << ", ";
+		}
+		cout << arr[i] << "";
+	}
+	cout << endl << endl;
+	
+	
+	// Delete
+	int del;
+	cout << "Pilih indeks data yang akan dihapus = ";
+	cin >> del;
+	
+	for (i = del-1; i < length-1; i++) {
+		arr[i] = arr[i+1];
+	}
+	
+	cout << "Output: ";
+	for (i = 0; i < length-1; i++) {
+		if (i > 0) {
+			cout << ", ";
+		}
+		cout << arr[i] << "";
+	}
+	cout << endl;
+	
 	
 	
 	return 0;
