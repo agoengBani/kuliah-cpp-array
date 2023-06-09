f6c9c060f4feb21a604ef3980332fbc414c9907d
Muhammad Agung Sya'bani <119343277+agoengBani@users.noreply.github.com>
2023-06-09 10:59:36 +0700

    Update pertemuan4.cpp

--- pertemuan4.cpp
+++ pertemuan4.cpp
@@ -8,49 +8,68 @@ int main() {
 	cout << "3. hapus data mhs" << endl;
 	cout << "4. tampilkan data mhs" << endl;
 	
-	int menu, i, tampil;
-	char input[50];
+	int menu, i, tampil, input;
+	
 	
 	cout << "Pilih menu: ";
 	cin >> menu;
 	
 	if (menu == 1) {
-		cout << "Input data mahasiswa: ";
+		cout << "Masukkan panjang input: ";
 		cin >> input;
-		// back to menu
-		cout << "Pilih menu: ";
-		cin >> menu;
-		if (menu == 2) {
-			cout << "\nEdit data mahasiswa \n";
-			cout << "Pilih indeks data yang ingin di ubah: ";
-			cin >> i;
-			cin >> input;
-			// back to menu
-			cout << "Pilih menu: ";
-			cin >> menu;
-			if (menu == 3) {
-				cout << "Hapus data mahasiswa";
-				cout << "Pilih indeks data yang ingin dihapus";
-				cin >> i;
-				// back to menu
-				cout << "Pilih menu: ";
-				cin >> menu;
-				if (menu == 4) {
-					cout << "Tampilkan data mahasiswa";
-					cin >> tampil;
-					// back to menu
-					cout << "Pilih menu: ";
-					cin >> menu;
-				}
-			}
-		}
-	} else {
-		cout << "Menu tidak ada";
+		
+		string arr[input];
+		
+	 	for (i = 0; i < input; i++) {
+	 		cout << "Input data ke " << i+1 << ": ";
+			cin >> arr[i];
+		}	
 	}
 	
+	cout << "Pilih menu: ";
+	cin >> menu;
 	
+	if (menu == 2) {
+		cout << "Pilih indeks yang akan diubah: ";
+		cin >> i;
+		
+		i = i-1;
+		
+//		string arr[input];
+		string arr[i];
+		
+		cout << "Edit data: ";
+		cin >> arr[i];
+	}
 	
+	cout << "Pilih menu: ";
+	cin >> menu;
 	
+	if (menu == 3) {
+		int del;
+		cout << "Pilih indeks data yang akan dihapus = ";
+		cin >> del;
+		
+		string arr[i];
+		for (i = del-1; i < input-1; i++ ) {
+			arr[i] = arr[i+1];
+		}
+	}
+	
+	cout << "Pilih menu: ";
+	cin >> menu;
+	
+	string arr[i];
+	if (menu == 4) {
+		cout << "Output: ";
+		for (i = 0; i < input-1; i++) {
+			if (i > 0) {
+				cout << ", ";
+			}
+			cout << arr[i] << "";
+		}
+		cout << endl;
+	}
 	
 	return 0;
 }
