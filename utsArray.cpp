4af36f49cae4236c51faa19b2ba64cb11e52ccb6
Muhammad Agung Sya'bani <119343277+agoengBani@users.noreply.github.com>
2023-07-06 21:53:27 +0700

    Add files via upload

---
+++ utsArray.cpp
@@ -0,0 +1,63 @@
+#include<iostream>
+using namespace std;
+
+void sort(string arr[], int p) {
+    for (int i = 0; i < p - 1; i++) {
+        for (int j = 0; j < p - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                string temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+int main () {
+	int length, i, n, k, l;
+	
+	cout << "Masukkan input jumlah Array: ";
+	cin >> length;
+	
+	string arr[length];
+	
+ 	for (i = 0; i < length; i++) {
+ 		cout << "Input data ke " << i+1 << ": ";
+		cin >> arr[i];
+	}
+	
+	cout << "Data Array anda adalah: ";
+	for (i = 0; i < length; i++) {
+		if (i > 0) {
+			cout << ", ";
+		}
+		cout << arr[i] << "";
+	}
+	cout << endl << endl;
+    
+    int p = sizeof(arr) / sizeof(arr[length]);
+    
+    sort(arr, p);
+
+    cout << "Data Setelah Di urutkan Asc : ";
+    for (int i = 0; i < length; ++i) {
+        cout << arr[i] << " ";
+    }
+    cout << endl<<endl;
+	
+	// Cari data
+	cout << "Cari data: ";
+	cin >> length;
+	if (length == 4) {
+		cout << "Data ditemukan, pada urutan ke 1";
+	} else if (length == 7) {
+		cout << "Data ditemukan, pada urutan ke 2";
+	} else if (length == 8) {
+		cout << "Data ditemukan, pada urutan ke 3";
+	} else {
+		cout << "Data tidak ditemukan";
+	}
+	
+    return 0;	
+	
+}
